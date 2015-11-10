a20beac4ca3da1d6352baf532023b409f6124230
Pirateninjawizard <adrianqsk@gmail.com>
2015-11-10 14:17:30 +0800

    Crude version. Goes up to 6

--- extension/cheat.js
+++ extension/cheat.js
@@ -1,12 +1,71 @@
 function bot(){
     var self = this;
-    var game = null;
+    self.game = null;
+    self.groundlim = GROUND_Y - 70;
+
+    self.checkGameEnd = function(){
+        // nothing can be done without the bird
+        if(bird==null || bird==undefined) return;
+        // we got game
+        if(self.game == null) self.game = bird.game;
+        // trigger reset
+        if(gameOver){
+            self.game.input.onTap.dispatch();
+            return;
+        }
+        // start the game
+        if(!gameStarted){
+            self.click();
+            return;
+        }
+    };
+
+    // deterministic play
+    self.play = function(){
+        if(!gameStarted) return;
+        // already flapped
+        if(bird.body.gravity.y==0) return;
+        var bleft = bird.bounds.x, bright = bird.bounds.x + bird.bounds.width, btop = bird.bounds.y, bbottom = bird.bounds.y + bird.bounds.height;
+        if(tubes.countLiving()==0) return self.clickGround(bbottom);
+
+        var closest = null;
+        tubes.forEachAlive(function(t){
+            var t = t.bounds, tleft = t.x, tright = t.x + t.width, ttop = t.y, tbottom = t.y + t.height;
+            // top tube
+            if(ttop<=0) return;
+            // already past
+            if(tright<bleft) return;
+            // init if null
+            if(closest==null){
+                closest = t;
+                return;
+            }
+            var xdist = closest.x + closest.width - bleft;
+            // if t is closer than current closest
+            if((tright - bleft) < xdist){
+                closest = t;
+            }
+        });
+
+        if(closest==null) return self.clickGround(bbottom);
+        // decide if we should click
+        if((closest.y - bbottom) <= 15) self.click();
+    };
+
+    // click based on how close we are to ground
+    self.clickGround = function(bbottom){
+        if(bbottom>=self.groundlim){
+            self.click();
+        }
+    }
+
+    self.click = function(){
+        self.game.input.onDown.dispatch();
+    }
 
     $(document).ready(function(){
-        $(document).click(function(){
-            // get game object
-            game = bird.game;
-        });
+        setInterval(self.checkGameEnd,30); 
+        setInterval(self.play,1); 
     });
 }
 // Inject bot into the page
